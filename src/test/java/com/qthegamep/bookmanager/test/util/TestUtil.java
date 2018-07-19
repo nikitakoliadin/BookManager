731f2a89eeab0342a15be175afac826421996203
Nikita Koliadin <qThegamEp@gmail.com>
2018-07-19 15:19:09 +0300

    Added TestUtil class

---
+++ src/test/java/com/qthegamep/bookmanager/test/util/TestUtil.java
@@ -0,0 +1,23 @@
+package com.qthegamep.bookmanager.test.util;
+
+import lombok.experimental.UtilityClass;
+
+import java.io.InputStream;
+import java.io.PrintStream;
+
+@UtilityClass
+public class TestUtil {
+
+    private final InputStream consoleInputStream = System.in;
+
+    private final PrintStream consolePrintStream = System.out;
+
+    /**
+     * This method set input and output to the console. It is used before and after each test where the input
+     * or output is substituted.
+     */
+    public void setInputOutputStreamToConsole() {
+        System.setIn(consoleInputStream);
+        System.setOut(consolePrintStream);
+    }
+}
