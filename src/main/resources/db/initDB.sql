4a3dc520962fdadad35602a619f64ee5d3c96d83
Nikita Koliadin <qThegamEp@gmail.com>
2018-07-20 14:49:51 +0300

    Added initDB.sql

---
+++ src/main/resources/db/initDB.sql
@@ -0,0 +1,14 @@
+DROP TABLE IF EXISTS bookmanager.books;
+
+CREATE TABLE `books` (
+  `id`        int(11)      NOT NULL AUTO_INCREMENT,
+  `name`      varchar(100) NOT NULL,
+  `author`    varchar(100) NOT NULL,
+  `printYear` int(4)       NOT NULL,
+  `isRead`    tinyint(4)   NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+)
+  ENGINE = InnoDB
+  AUTO_INCREMENT = 1
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_0900_ai_ci;
