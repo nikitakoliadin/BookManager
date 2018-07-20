7d310e3abeb8a9ee92869c1263212a263db678b8
Nikita Koliadin <qThegamEp@gmail.com>
2018-07-20 15:37:52 +0300

    Edited query in initDB.sql

--- src/main/resources/db/initDB.sql
+++ src/main/resources/db/initDB.sql
@@ -1,12 +1,12 @@
 DROP TABLE IF EXISTS bookmanager.books;
 
-CREATE TABLE `books` (
-  `id`        int(11)      NOT NULL AUTO_INCREMENT,
-  `name`      varchar(100) NOT NULL,
-  `author`    varchar(100) NOT NULL,
-  `printYear` int(4)       NOT NULL,
-  `isRead`    tinyint(4)   NOT NULL DEFAULT '0',
-  PRIMARY KEY (`id`)
+CREATE TABLE books (
+  id        int(11)      NOT NULL AUTO_INCREMENT,
+  name      varchar(100) NOT NULL,
+  author    varchar(100) NOT NULL,
+  printYear int(4)       NOT NULL,
+  isRead    tinyint(1)   NOT NULL DEFAULT 0,
+  PRIMARY KEY (id)
 )
   ENGINE = InnoDB
   AUTO_INCREMENT = 1
