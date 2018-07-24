d4cc0b4f1ca762ae5889dcb5e48af948aad2e491
Nikita Koliadin <qThegamEp@gmail.com>
2018-07-24 16:42:20 +0300

    Added populateDB.sql script

---
+++ src/main/resources/db/populateDB.sql
@@ -0,0 +1,5 @@
+DELETE FROM BOOKS;
+
+INSERT INTO BOOKS (NAME, AUTOR, PRINTYEAR, ISREAD) VALUES
+  ('Thinking In Java 4 Edition', 'Bruce Eckel', 2017, TRUE),
+  ('Pro Spring', 'Clarence Ho, Rob Harrop, Chris Schaefer', 2014, FALSE);
--- src/main/resources/db/populateDB_MySQL.sql
+++
@@ -1,5 +0,0 @@
-DELETE FROM bookmanager.books;
-
-INSERT INTO bookmanager.books (name, author, printYear, isRead) VALUES
-  ('Thinking In Java 4 Edition', 'Bruce Eckel', 2017, 1),
-  ('Pro Spring', 'Clarence Ho, Rob Harrop, Chris Schaefer', 2014, 0);
