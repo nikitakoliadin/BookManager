c3fc8ddf73d8e7806843097f6ad92cd82b4d07c7
Nikita Koliadin <qThegamEp@gmail.com>
2018-07-26 19:00:13 +0300

    Updated AUTHOR field

--- src/main/resources/db/h2/populateDB.sql
+++ src/main/resources/db/h2/populateDB.sql
@@ -1,3 +1,3 @@
-INSERT INTO PUBLIC.BOOKS (NAME, AUTOR, PRINT_YEAR, IS_READ)
+INSERT INTO PUBLIC.BOOKS (NAME, AUTHOR, PRINT_YEAR, IS_READ)
 VALUES ('Thinking In Java 4 Edition', 'Bruce Eckel', 2017, TRUE),
        ('Pro Spring', 'Clarence Ho, Rob Harrop, Chris Schaefer', 2014, FALSE);
