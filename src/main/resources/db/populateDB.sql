dcd5aafaa7f9e1873dbf231b6629f47e3f66d0ba
Nikita Koliadin <qThegamEp@gmail.com>
2018-07-26 13:28:55 +0300

    Updated populateDB.sql

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,5 +1,6 @@
-DELETE FROM PUBLIC.BOOKS;
+DELETE
+FROM PUBLIC.BOOKS;
 
-INSERT INTO PUBLIC.BOOKS (NAME, AUTOR, PRINTYEAR, ISREAD) VALUES
-  ('Thinking In Java 4 Edition', 'Bruce Eckel', 2017, TRUE),
-  ('Pro Spring', 'Clarence Ho, Rob Harrop, Chris Schaefer', 2014, FALSE);
+INSERT INTO PUBLIC.BOOKS (NAME, AUTOR, PRINT_YEAR, IS_READ)
+VALUES ('Thinking In Java 4 Edition', 'Bruce Eckel', 2017, TRUE),
+       ('Pro Spring', 'Clarence Ho, Rob Harrop, Chris Schaefer', 2014, FALSE);
