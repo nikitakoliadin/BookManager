e85b1445929b516ae60ecc91023a296ffaf68752
Nikita Koliadin <qThegamEp@gmail.com>
2018-07-26 18:27:42 +0300

    Updated h2/populateDB.sql path

--- src/main/resources/db/h2/populateDB.sql
+++ src/main/resources/db/h2/populateDB.sql
@@ -1,6 +1,3 @@
-DELETE
-FROM PUBLIC.BOOKS;
-
 INSERT INTO PUBLIC.BOOKS (NAME, AUTOR, PRINT_YEAR, IS_READ)
 VALUES ('Thinking In Java 4 Edition', 'Bruce Eckel', 2017, TRUE),
        ('Pro Spring', 'Clarence Ho, Rob Harrop, Chris Schaefer', 2014, FALSE);
