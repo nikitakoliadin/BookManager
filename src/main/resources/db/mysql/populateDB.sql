71a0c17b70448befbfbaa06326abcd86d8bfbab2
Nikita Koliadin <qThegamEp@gmail.com>
2018-07-26 19:04:26 +0300

    Added mysql/populateDB.sql

---
+++ src/main/resources/db/mysql/populateDB.sql
@@ -0,0 +1,3 @@
+INSERT INTO `bookmanager`.`books` (NAME, AUTHOR, PRINT_YEAR, IS_READ)
+VALUES ('Thinking In Java 4 Edition', 'Bruce Eckel', 2017, 1),
+       ('Pro Spring', 'Clarence Ho, Rob Harrop, Chris Schaefer', 2014, 0);
