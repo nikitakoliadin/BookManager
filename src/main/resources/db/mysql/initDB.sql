5cf2e8d3c0a967b38d2125c82c2615f0d6c56ddf
Nikita Koliadin <qThegamEp@gmail.com>
2018-07-26 18:51:45 +0300

    Added mysql/initDB.sql

---
+++ src/main/resources/db/mysql/initDB.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE IF NOT EXISTS bookmanager;
+
+DROP TABLE IF EXISTS `bookmanager`.`books`;
+
+CREATE TABLE `bookmanager`.`books` (
+  `ID`         INT          NOT NULL AUTO_INCREMENT,
+  `NAME`       VARCHAR(255) NOT NULL,
+  `AUTHOR`     VARCHAR(255) NOT NULL,
+  `PRINT_YEAR` INT(4)       NOT NULL,
+  `IS_READ`    TINYINT(1)   NOT NULL DEFAULT 0,
+  CONSTRAINT `BOOKS_PRIMARY_KEY` PRIMARY KEY (`ID`)
+)
+  ENGINE = InnoDB
+  AUTO_INCREMENT = 1
+  DEFAULT CHARSET = utf8
+  COLLATE = utf8_general_ci;
