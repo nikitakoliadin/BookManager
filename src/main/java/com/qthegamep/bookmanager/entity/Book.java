dc800fff5be8403b5cdd1a8db3730b9644897d56
Nikita Koliadin <qThegamEp@gmail.com>
2018-07-29 17:23:31 +0300

    Deleted all args constructor, updated JavaDoc

--- src/main/java/com/qthegamep/bookmanager/entity/Book.java
+++ src/main/java/com/qthegamep/bookmanager/entity/Book.java
@@ -1,17 +1,12 @@
 package com.qthegamep.bookmanager.entity;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 /**
  * This class is an entity.
- * There is an no args and all args constructors, getters and setters for fields,
- * override equals, hashcode and toString methods.
+ * There is an no args constructor, getters and setters for fields, override equals, hashcode and toString methods.
  */
 @Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class Book {
 
     private int id;
