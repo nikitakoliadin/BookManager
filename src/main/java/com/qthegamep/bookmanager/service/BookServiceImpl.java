ee7ec1c74dc98d5ef6998e79ee8f8caaa02454b0
Nikita Koliadin <qThegamEp@gmail.com>
2018-08-06 11:17:31 +0300

    Added throws exception, updated JavaDoc

--- src/main/java/com/qthegamep/bookmanager/service/BookServiceImpl.java
+++ src/main/java/com/qthegamep/bookmanager/service/BookServiceImpl.java
@@ -7,6 +7,7 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 
+import java.sql.SQLException;
 import java.util.List;
 
 /**
@@ -24,9 +25,10 @@ public class BookServiceImpl implements BookService {
      * If the entity has id and this entity already exists in the database entity will be updated.
      *
      * @param book is the entity object that will be added to the database.
+     * @throws SQLException of work with the database.
      */
     @Override
-    public void add(Book book) {
+    public void add(Book book) throws SQLException {
 
     }
 
@@ -35,9 +37,10 @@ public class BookServiceImpl implements BookService {
      * If any entity has id and this entity already exists in the database entity will be updated.
      *
      * @param books is the list of entities objects that will be added to the database.
+     * @throws SQLException of work with the database.
      */
     @Override
-    public void addAll(List<? extends Book> books) {
+    public void addAll(List<? extends Book> books) throws SQLException {
 
     }
 
@@ -46,9 +49,10 @@ public class BookServiceImpl implements BookService {
      *
      * @param id is the parameter by which the entity object will be returned.
      * @return book entity object.
+     * @throws SQLException of work with the database.
      */
     @Override
-    public Book getById(int id) {
+    public Book getById(int id) throws SQLException {
         return null;
     }
 
@@ -57,9 +61,10 @@ public class BookServiceImpl implements BookService {
      *
      * @param name is the parameter by which the list of entities objects will be returned.
      * @return list of books entities objects.
+     * @throws SQLException of work with the database.
      */
     @Override
-    public List<Book> getByName(String name) {
+    public List<Book> getByName(String name) throws SQLException {
         return null;
     }
 
@@ -68,9 +73,10 @@ public class BookServiceImpl implements BookService {
      *
      * @param author is the parameter by which the list of entities objects will be returned.
      * @return list of books entities objects.
+     * @throws SQLException of work with the database.
      */
     @Override
-    public List<Book> getByAuthor(String author) {
+    public List<Book> getByAuthor(String author) throws SQLException {
         return null;
     }
 
@@ -79,9 +85,10 @@ public class BookServiceImpl implements BookService {
      *
      * @param printYear is the parameter by which the list of entities objects will be returned.
      * @return list of books entities objects.
+     * @throws SQLException of work with the database.
      */
     @Override
-    public List<Book> getByPrintYear(int printYear) {
+    public List<Book> getByPrintYear(int printYear) throws SQLException {
         return null;
     }
 
@@ -90,9 +97,10 @@ public class BookServiceImpl implements BookService {
      *
      * @param isRead is the parameter by which the list of entities objects will be returned.
      * @return list of books entities objects.
+     * @throws SQLException of work with the database.
      */
     @Override
-    public List<Book> getByIsRead(boolean isRead) {
+    public List<Book> getByIsRead(boolean isRead) throws SQLException {
         return null;
     }
 
@@ -100,9 +108,10 @@ public class BookServiceImpl implements BookService {
      * This service method implements returning list of all books entities objects from the database.
      *
      * @return list of books entities objects.
+     * @throws SQLException of work with the database.
      */
     @Override
-    public List<Book> getAll() {
+    public List<Book> getAll() throws SQLException {
         return null;
     }
 
@@ -110,9 +119,10 @@ public class BookServiceImpl implements BookService {
      * This service method implements updating book entity object in the database.
      *
      * @param book is the new entity that will be added to the database instead of the old one.
+     * @throws SQLException of work with the database.
      */
     @Override
-    public void update(Book book) {
+    public void update(Book book) throws SQLException {
 
     }
 
@@ -120,9 +130,10 @@ public class BookServiceImpl implements BookService {
      * This service method implements updating list of books entities objects in the database.
      *
      * @param books is the new entities that will be added to the database instead of the old ones.
+     * @throws SQLException of work with the database.
      */
     @Override
-    public void updateAll(List<? extends Book> books) {
+    public void updateAll(List<? extends Book> books) throws SQLException {
 
     }
 
@@ -130,9 +141,10 @@ public class BookServiceImpl implements BookService {
      * This service method implements deleting book entity object from the database.
      *
      * @param book is the entity that will be deleted from the database.
+     * @throws SQLException of work with the database.
      */
     @Override
-    public void remove(Book book) {
+    public void remove(Book book) throws SQLException {
 
     }
 
@@ -140,9 +152,10 @@ public class BookServiceImpl implements BookService {
      * This service method implements deleting list of books entities objects from the database.
      *
      * @param books is the entities that will be deleted from the database.
+     * @throws SQLException of work with the database.
      */
     @Override
-    public void removeAll(List<? extends Book> books) {
+    public void removeAll(List<? extends Book> books) throws SQLException {
 
     }
 }
